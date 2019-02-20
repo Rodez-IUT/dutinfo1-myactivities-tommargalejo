7847b05ddd2cf0e921834291f947dd65cef4b41f
Tom Margalejo <tom.margalejo@iut-rodez.fr>
2019-02-20 13:25:35 +0100

    GIT Fix 2.2 function to add new activity OK

--- src/main/resources/db/migration/R__Function_to_delete_old_activities.sql
+++ src/main/resources/db/migration/R__Function_to_delete_old_activities.sql
@@ -1,6 +1,5 @@
-CREATE OR REPLACE FUNCTION delete_activities_older_than(old_date date) RETURNS int AS $$
+CREATE OR REPLACE FUNCTION delete_activities_older_than(old_date date) RETURNS void AS $$
    DELETE
    FROM public.activity
    WHERE modification_date < old_date
-   RETURNING 0
 $$ LANGUAGE SQL;
