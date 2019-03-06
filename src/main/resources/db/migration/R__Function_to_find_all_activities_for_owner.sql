dff2e9366bb137a5bd403c37173233b5de058217
Tom Margalejo <tom.margalejo@iut-rodez.fr>
2019-03-06 16:06:29 +0100

    Fix 3.2 function to find all activities for a given owner

--- src/main/resources/db/migration/R__Function_to_find_all_activities_for_owner.sql
+++ src/main/resources/db/migration/R__Function_to_find_all_activities_for_owner.sql
@@ -1,7 +1,17 @@
-CREATE OR REPLACE FUNCTION find_all_activities_for_owner(owner character varying(500)) RETURNS SETOF activity AS $$
-  SELECT * 
-   			  FROM activity
-   			  WHERE owner_id = ( SELECT DISTINCT id
-   			  					 FROM "user"
-   			  					 WHERE user = owner ); 
+
+CREATE OR REPLACE FUNCTION find_all_activities_for_owner(ownername varchar(500)) RETURNS SETOF activity AS $$
+  SELECT act.* FROM activity act JOIN "user" owner ON owner_id = owner.id WHERE owner.username = ownername
 $$ LANGUAGE SQL;
+
+
+/*
+CREATE OR REPLACE FUNCTION find_all_activities_for_owner(owner character varying(500)) RETURNS SETOF activity AS $$
+  DECLARE
+  	userId integer;
+  BEGIN
+  	userId := 1
+  	--userId := select id from user where username = owner
+  	--select * from activity where owner_id == userId
+  END activity;
+$$ LANGUAGE plpgsql;
+*/
