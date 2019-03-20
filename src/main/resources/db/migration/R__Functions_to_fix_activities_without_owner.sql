395b1fa19d201a347e96be8f2e50f8943359323c
Tom Margalejo <tom.margalejo@iut-rodez.fr>
2019-03-20 15:01:42 +0100

    Sauvegarde espace de travail

--- src/main/resources/db/migration/R__Functions_to_fix_activities_without_owner.sql
+++ src/main/resources/db/migration/R__Functions_to_fix_activities_without_owner.sql
@@ -18,30 +18,33 @@ CREATE OR REPLACE FUNCTION fix_activities_without_owner() RETURNS record AS $$
 	UPDATE activity SET owner_id = get_default_owner() WHERE owner_id IS NULL
 	RETURNING activity.id
 $$ LANGUAGE plpgsql;*/
-CREATE OR REPLACE FUNCTION get_default_owner() RETURNS "user" AS &&
+
+
+CREATE OR REPLACE FUNCTION get_default_owner() RETURNS "user" AS $$
 	DECLARE
 		defaultOwner "user"%rowtype;
-		defaultOwnerUsername carchar(500) := 'Default Owner';
+		defaultOwnerUsername varchar(500) := 'Default Owner';
 	BEGIN
 		select * into defaultOwner from "user"
 			where username = defaultOwnerUsername;
 		if not found then
-			insert ito "user" (id, username)
+			insert into "user" (id, username)
 				values (nextval('id_generator'), defaultOwnerUsername);
 			select * into defaultOwner from "user"
 				where username = defaultOwnerUsername;
-		end if
+		end if;
 		return defaultOwner;
-	END
-&& LANGUAGE plpgsql
+	END;
+$$ LANGUAGE plpgsql;
 
-/*CREATE OR REPLACE FUNCTION fiw_activities_without_owner() RETURNS SETOF activity &&
+CREATE OR REPLACE FUNCTION fix_activities_without_owner() RETURNS SETOF activity $$
 	DECLARE
-	
+		
 	BEGIN
-	
-	END
-&& LANGUAGE plpgsql*/
+		UPDATE activity SET owner = (SELECT id FROM user WHERE username = 'Default Owner') WHERE owner_id IS NULL
+		
+	END;
+$$ LANGUAGE plpgsql;
 
 
 
